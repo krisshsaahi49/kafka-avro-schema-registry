17644a14df7236e506c277c7e7469e784f1e22d0
krisshsaahi49 <krisshsaahi@outlook.com>
2025-02-10 11:59:09 -0500

    first commit

---
+++ src/main/java/com/kafka/kafka_schema_registry/KafkaSchemaRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.kafka_schema_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSchemaRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSchemaRegistryApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kafka/kafka_schema_registry/controller/EventController.java
@@ -0,0 +1,20 @@
+package com.kafka.kafka_schema_registry.controller;
+
+import com.kafka.kafka_schema_registry.dto.Employee;
+import com.kafka.kafka_schema_registry.producer.KafkaAvroProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+    @Autowired
+    private KafkaAvroProducer producer;
+
+    @PostMapping("/events")
+    public String sendMessage(@RequestBody Employee employee) {
+        producer.send(employee);
+        return "message published !";
+    }
+}
